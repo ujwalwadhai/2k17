b4c2cf97e619f052dd9629cd98d0f4bf63c055f7
Ujwal W. <wadhaiujwal@gmail.com>
2025-05-12 10:19:46 +0530

    Minor changes in design and database structure

--- models/User.js
+++ models/User.js
@@ -53,203 +53,582 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password); // Compare entered password with hashed password
 };
 
-/*
+var User = mongoose.model('Users', userSchema);
 
 const preUsersData = [
   {
     "name": "Aditi Khobragade",
     "dob": "07/10/2006",
-    "password": "07/10/2006"
-  },
-  {
-    "name": "Aditi Zade",
-    "dob": "24/10/2006",
-    "password": "24/10/2006"
-  },
-  {
-    "name": "Akshita Ray",
-    "dob": "01/07/2006",
-    "password": "01/07/2006"
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
     "name": "Ashish Sopankar",
     "dob": "16/07/2006",
-    "password": "16/07/2006"
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
     "name": "Bhavishya Rangari",
     "dob": "04/09/2006",
-    "password": "04/09/2006"
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
     "name": "Himanshu Mangam",
     "dob": "18/12/2005",
-    "password": "18/12/2005"
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
     "name": "Ketan Mongarkar",
     "dob": "05/07/2005",
-    "password": "05/07/2005"
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
-    "name": "Khushi Ramteke",
-    "dob": "31/03/2007",
-    "password": "31/03/2007"
+    "name": "Akshita Ray",
+    "dob": "01/07/2006",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
-    "name": "Manasvi Borkar",
-    "dob": "01/05/2006",
-    "password": "01/05/2006"
+    "name": "Aditi Zade",
+    "dob": "24/10/2006",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
     "name": "Naina Khadsang",
     "dob": "28/06/2006",
-    "password": "28/06/2006"
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
     "name": "Prajwal Chaudhari",
     "dob": "14/07/2005",
-    "password": "14/07/2005"
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
     "name": "Prajwali Tajane",
     "dob": "07/09/2006",
-    "password": "07/09/2006"
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
     "name": "Prajyot Meshram",
     "dob": "21/05/2006",
-    "password": "21/05/2006"
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
     "name": "Prajyot Raut",
     "dob": "06/10/2005",
-    "password": "06/10/2005"
+    "profilePicture": "",
+    "role": "moderator",
+    "year": "2020-24"
   },
   {
     "name": "Ritik Madavi",
     "dob": "28/04/2006",
-    "password": "28/04/2006"
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
     "name": "Rutuja Kamdi",
     "dob": "08/07/2006",
-    "password": "08/07/2006"
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
     "name": "Samiksha Chunarkar",
     "dob": "28/04/2006",
-    "password": "28/04/2006"
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
     "name": "Samyak Sakhare",
     "dob": "22/10/2005",
-    "password": "22/10/2005"
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
     "name": "Shankar Bharde",
     "dob": "08/09/2006",
-    "password": "08/09/2006"
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
     "name": "Sneha Togare",
     "dob": "01/01/2006",
-    "password": "01/01/2006"
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
     "name": "Trupti Raut",
     "dob": "28/10/2005",
-    "password": "28/10/2005"
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
     "name": "Ujwal Wadhai",
     "dob": "13/09/2006",
-    "password": "13/09/2006"
+    "profilePicture": "/images/ujwal_profile.jpg",
+    "role": "admin",
+    "year": "2017-24"
   },
   {
-    "name": "Vivekanand Puram",
-    "dob": "08/02/2006",
-    "password": "08/02/2006"
+    "name": "Khushi Ramteke",
+    "dob": "31/03/2007",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
     "name": "Aditya Patil",
     "dob": "24/02/2006",
-    "password": "24/02/2006"
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Manasvi Borkar",
+    "dob": "01/05/2006",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
     "name": "Bhairavi Gedam",
     "dob": "01/10/2006",
-    "password": "01/10/2006"
-  },
-  {
-    "name": "Manali Humane",
-    "dob": "25/10/2006",
-    "password": "25/10/2006"
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
     "name": "Manthan Suryawanshi",
     "dob": "12/03/2006",
-    "password": "12/03/2006"
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
     "name": "Monali Chandanbawane",
     "dob": "18/10/2006",
-    "password": "18/10/2006"
-  },
-  {
-    "name": "Paresh Nimje",
-    "dob": "13/04/2006",
-    "password": "13/04/2006"
-  },
-  {
-    "name": "Pranjal Madkam",
-    "dob": "26/10/2005",
-    "password": "26/10/2005"
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
     "name": "Rhutik Kunbhare",
     "dob": "22/03/2006",
-    "password": "22/03/2006"
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Paresh Nimje",
+    "dob": "13/04/2006",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Pranjal Madkam",
+    "dob": "26/10/2005",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
     "name": "Sejal Ragit",
     "dob": "28/04/2006",
-    "password": "28/04/2006"
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
     "name": "Shrishant Bagale",
     "dob": "06/11/2005",
-    "password": "06/11/2005"
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
     "name": "Shrushti Khandare",
     "dob": "25/04/2006",
-    "password": "25/04/2006"
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
     "name": "Suhani Chaudhary",
     "dob": "31/05/2006",
-    "password": "31/05/2006"
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
     "name": "Vaishnavi Shedala",
     "dob": "05/11/2006",
-    "password": "05/11/2006"
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
     "name": "Vedanti Fukat",
     "dob": "18/10/2006",
-    "password": "18/10/2006"
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
     "name": "Yash Magare",
     "dob": "02/10/2006",
-    "password": "02/10/2006"
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   },
   {
     "name": "Shraddha Pakhmode",
     "dob": "27/10/2006",
-    "password": "27/10/2006"
+    "profilePicture": "",
+    "role": "user",
+    "year": "2020-24"
+  },
+  {
+    "name": "Vivekanand Puram",
+    "dob": "08/02/2006",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Manali Humane",
+    "dob": "25/10/2006",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Sagar Barekar",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Milind Ghodmare",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Vaibhav Nannaware",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Piyush Shende",
+    "dob": "14/10/2006",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Krish Ninawe",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Shrawan Borkar",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Sanskar Kayarkar",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Dweep Raut",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Manas Dawande",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Dhruv Meshram",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Rohit Thakare",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Aryan Shelke",
+    "dob": "16/12/2006",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Smith Navale",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Aharth Nimgade",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Sneha Bhagat",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2020-24"
+  },
+  {
+    "name": "Sanika Bawane",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Pooja Moon",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Purva Patil",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Pradnya Chahande",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Janavi Magare",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Chaitali Shende",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Akansha Satpute",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Palak Thakare",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Ayusha Urade",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Priyanka Surpam",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Sanika Nagrale",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Parikshit Doye",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Harshal Hulke",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Sanskar Hanwate",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Kshitij Urade",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Sahil Kurzekar",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Krish Dange",
+    "dob": "27/06/2006",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Krishna Jadhav",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Sudhanshu Randhai",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Hrichita Pal",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Vedika Shende",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2020-24"
+  },
+  {
+    "name": "Kalyani Gedam",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Neha Gedam",
+    "dob": "03/05/2006",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Kimaya Pawade",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Nandini Ladwe",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Shrawani Sahare",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Spandan Bansod",
+    "dob": "",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
+  },
+  {
+    "name": "Manasi Naoghade",
+    "dob": "31/01/2006",
+    "profilePicture": "",
+    "role": "user",
+    "year": "2017-24"
   }
 ]
 
@@ -259,7 +638,20 @@ try {
       name: user.name,
       username: user.name.replace(/\s+/g, '').toLowerCase(),
       dob: user.dob,
-      password: user.dob
+      password: user.dob,
+      email: "",
+      phone: "",
+      profilePicture: user.profilePicture,
+      role: user.role,
+      year: user.year,
+      bio: "",
+      socialLinks: {
+        instagram: "",
+        linkedin: "",
+        github: "",
+        facebook: "",
+        other: ""
+      }
     })
     userData.save();
   })
@@ -268,9 +660,9 @@ try {
 } catch (err) {
   console.error('Insert error:', err);
   mongoose.connection.close();
-} */
+}
 
-var User = mongoose.model('Users', userSchema);
+
 
 
 
